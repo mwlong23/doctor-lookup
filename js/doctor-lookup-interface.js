73612d04de32d1f7af72e4962be768871c6ef35e
Mitch Long <meechllada@gmail.com>
2017-10-23 15:57:37 -0700

    fixes api connection

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -1,8 +1,8 @@
-var api_Key = require('./../.env').apiKey;
+var apiKey = require('./../.env').apiKey;
 
-// var api_Key = "3cf0ffbb88fae132f82086fd2704ace2"
 
-var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + api_Key;
+
+var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + apiKey;
 
 
 
