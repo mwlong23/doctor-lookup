d4eb327ef81095480e6343e2ed0c024160d6ca12
Mitchell Long <meechllada@gmail.com>
2017-10-24 16:35:57 -0700

    combine business and interface logic for testing

--- build/js/app.js
+++ build/js/app.js
@@ -1,19 +1,16 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-exports.apiKey = "3cf0ffbb88fae132f82086fd2704ace2"
-
-},{}],2:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.DoctorSearch = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _env = require('./../.env');
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// import { DoctorSearch } from './../js/doctor-lookup.js'
+var doctorSearch = new DoctorSearch();
 
 var DoctorSearch = exports.DoctorSearch = function () {
   function DoctorSearch() {
@@ -23,7 +20,7 @@ var DoctorSearch = exports.DoctorSearch = function () {
   _createClass(DoctorSearch, [{
     key: 'search',
     value: function search(searchTerm, category) {
-      return $.get('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + _env.apiKey);
+      return $.get('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + apiKey);
     }
   }, {
     key: 'find',
@@ -35,15 +32,9 @@ var DoctorSearch = exports.DoctorSearch = function () {
   return DoctorSearch;
 }();
 
-},{"./../.env":1}],3:[function(require,module,exports){
-'use strict';
-
-var _doctorLookup = require('./../js/doctor-lookup.js');
-
-var doctorSearch = new _doctorLookup.DoctorSearch();
-
 $(document).ready(function () {
-  $('#search').submit(function () {
+  $('#search').submit(function (e) {
+    e.preventDefault();
     var category = $('#category').val();
     var searchCriteria = $('#search-criteria').val();
   });
@@ -55,4 +46,4 @@ $(document).ready(function () {
 //     document.getElementById('content-placeholder').innerHTML = template(data);
 // });
 
-},{"./../js/doctor-lookup.js":2}]},{},[3]);
+},{}]},{},[1]);
--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -1,9 +1,21 @@
-import { DoctorSearch } from './../js/doctor-lookup.js'
+// import { DoctorSearch } from './../js/doctor-lookup.js'
 const doctorSearch = new DoctorSearch();
 
 
+export class DoctorSearch{
+  search(searchTerm, category){
+    return $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${apiKey}`)
+  }
+
+  find(url){
+    return $.get(url);
+  }
+}
+
+
 $(document).ready(function(){
-  $('#search').submit(function(){
+  $('#search').submit(function(e){
+    e.preventDefault();
     let category = $('#category').val();
     let searchCriteria = $('#search-criteria').val();
 
